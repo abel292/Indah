06cc9ffe2f4c521b42d089f9793f287cd123a3fe
abel <abel.aguilar292@gmail.com>
2020-08-26 03:24:57 -0300

    fluides en trasition fragment

--- app/src/main/java/com/android_abel/indah/_view_ui/activities/HomeActivity.kt
+++ app/src/main/java/com/android_abel/indah/_view_ui/activities/HomeActivity.kt
@@ -20,7 +20,7 @@ import com.android_abel.indah.utils.CustomsConstantes
 import kotlinx.android.synthetic.main.activity_home.*
 
 
-class HomeActivity : BaseActivity(), View.OnTouchListener {
+class HomeActivity : BaseActivity() {
 
     lateinit var navController: NavController
 
@@ -54,22 +54,6 @@ class HomeActivity : BaseActivity(), View.OnTouchListener {
     }
 
     override fun initListeners() {
-        motion_layout.setTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
-            }
-
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-            }
-
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
-            }
-
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                if (currentAnim == TAB_REARRANGEMENT_ANIM && motion_layout.progress == 1f && transitionTabIndex != 0) {
-                    currentAnim = TAB_SLIDE_ANIM
-                }
-            }
-        })
 
         btn_back.setOnClickListener {
             if (currentAnim == TAB_SLIDE_ANIM) {
@@ -86,14 +70,13 @@ class HomeActivity : BaseActivity(), View.OnTouchListener {
         first_image.setOnClickListener {
             navController.navigate(R.id.productosFragment)
             if (currentAnim == TAB_REARRANGEMENT_ANIM) {
+                transitionTabIndex = 0
                 lastEndTransition = R.id.endFirst
                 motion_layout.setTransition(R.id.start, R.id.endFirst)
                 motion_layout.setTransitionDuration(700)
                 motion_layout.transitionToEnd()
-                setTabIndicator()
-                transitionTabIndex = 0
                 currentAnim = TAB_SLIDE_ANIM
-
+                setTabIndicator()
             }
         }
 
@@ -103,11 +86,11 @@ class HomeActivity : BaseActivity(), View.OnTouchListener {
             navController.navigate(R.id.ventasFragment, bundle)
 
             if (currentAnim == TAB_REARRANGEMENT_ANIM) {
+                transitionTabIndex = 1
                 lastEndTransition = R.id.endSecond
                 motion_layout.setTransition(R.id.start, R.id.endSecond)
                 motion_layout.setTransitionDuration(700)
                 motion_layout.transitionToEnd()
-                transitionTabIndex = 1
                 currentAnim = TAB_SLIDE_ANIM
                 setTabIndicator()
             }
@@ -124,65 +107,6 @@ class HomeActivity : BaseActivity(), View.OnTouchListener {
                 currentAnim = TAB_SLIDE_ANIM
                 setTabIndicator()
 
-            }
-        }
-
-        fourth_image.setOnClickListener {
-            if (currentAnim == TAB_REARRANGEMENT_ANIM) {
-
-                transitionTabIndex = 3
-                //no hace nada por que esta oculto
-
-                lastEndTransition = R.id.endFourth
-                motion_layout.setTransition(R.id.start, R.id.endFourth)
-                motion_layout.setTransitionDuration(700)
-                motion_layout.transitionToEnd()
-
-                currentAnim = TAB_SLIDE_ANIM
-                setTabIndicator()
-            }
-        }
-
-        touch_view.setOnTouchListener(this) // Swipe Area for controlling touch movement
-    }
-
-    override fun onTouch(v: View, event: MotionEvent): Boolean {
-        val action: Int = event.action
-
-        if (currentAnim == TAB_SLIDE_ANIM) {
-            currentAnim = TAB_REARRANGEMENT_ANIM
-
-            motion_layout.setTransition(R.id.start, lastEndTransition)
-            motion_layout.progress = 1f
-            animProgress = 1f
-        }
-
-        return when (action) {
-            MotionEvent.ACTION_MOVE -> {
-                animProgress += (oldY - event.rawY) * 0.0015f // Implementing swipe up and down behavior
-
-                // Clamp to maximum animation value
-                if (animProgress > 1f) {
-                    animProgress = 1f
-                }
-
-                // Clamp to minimum animation value
-                if (animProgress < 0) {
-                    animProgress = 0f
-                }
-                motion_layout.progress = animProgress
-                oldY = event.rawY.toInt()
-
-                true
-            }
-            MotionEvent.ACTION_UP -> {
-                oldY = event.rawY.toInt()
-                returnToRelativePosition()
-                true
-            }
-            else -> {
-                oldY = event.rawY.toInt()
-                true
             }
         }
     }
@@ -214,41 +138,23 @@ class HomeActivity : BaseActivity(), View.OnTouchListener {
                 first_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.active))
                 second_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
                 third_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
-                fourth_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
             }
             1 -> {
                 first_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
                 second_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.active))
                 third_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
-                fourth_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
             }
             2 -> {
                 first_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
                 second_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
                 third_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.active))
-                fourth_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
             }
             3 -> {
                 first_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
                 second_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
                 third_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.inActive))
-                fourth_indicator.setBackgroundColor(ContextCompat.getColor(this, R.color.active))
             }
         }
     }
 
-    private fun returnToRelativePosition() {
-        if (animProgress <= 0f || animProgress >= 1f) {
-            return
-        }
-        if (animProgress < 0.25) {
-            animProgress = 0f
-            motion_layout.transitionToStart()
-            currentAnim = TAB_REARRANGEMENT_ANIM
-        } else {
-            animProgress = 1f
-            motion_layout.transitionToEnd()
-            currentAnim = TAB_SLIDE_ANIM
-        }
-    }
 }
